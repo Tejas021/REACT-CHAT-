3ba4e50b6e83e8a20b4a4949bb7a52d7d9f9aafa
Tejas Kolwankar <tejaskolwankar021@gmail.com>
2021-08-29 11:52:37 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Chat from "./components/chat/Chat";
+import Home from "./components/home/Home";
+import {UserContext} from "./UserContext";
+import { BrowserRouter as Router,Switch,Route } from "react-router-dom";
+import {useState,useEffect} from "react"
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import Cookies from 'js-cookie';
+
 
 function App() {
-  return (
+
+
+
+
+
+const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const cookie = Cookies.get()
+    // document.cookie="user=Tejas";
+
+    const verifyUser = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/verifyuser', {
+        method:"POST",  
+        credentials: 'include',
+          headers: { 'Content-Type': 'application/json' },
+          body:JSON.stringify({cookie})
+        });
+        const data = await res.json();
+        setUser(data);
+      } catch (error) {
+        console.log(error)
+      }
+
+
+    }
+    verifyUser()
+
+
+  }, [])
+
+
+return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContext.Provider value={{user,setUser}} >
+    
+<Switch>
+  <Route exact path="/" component={Home}/>
+
+  <Route path="/chat/:room_id/:room_name" component={Chat}/>
+  <Route path="/signup" component={SignUp} />
+  <Route path="/signin" component={SignIn} />
+  <Route/>
+</Switch>
+
+
+
+      </UserContext.Provider>
+ 
+
+
     </div>
+    </Router>
   );
 }
 
